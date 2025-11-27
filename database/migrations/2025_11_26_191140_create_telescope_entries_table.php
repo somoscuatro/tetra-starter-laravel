864b72a2457bf3d2202147c26a479be1d13a611c
Luca Pipolo <luca@somoscuatro.es>
2025-11-27 15:22:58 +0100

    fix: type error reported by Larastan

--- database/migrations/2025_11_26_191140_create_telescope_entries_table.php
+++ database/migrations/2025_11_26_191140_create_telescope_entries_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function getConnection(): ?string
     {
-        return config('telescope.storage.database.connection');
+        $connection = config('telescope.storage.database.connection');
+
+        return is_string($connection) ? $connection : null;
     }
 
     /**
